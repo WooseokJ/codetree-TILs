dad1cd657632f9d08c33193c6edd3a0f238a3748
Wooseok <wooseokbird@gmail.com>
2024-02-01 18:15:09 +0900

    Title: 문자열 받아 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240201/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next(); // 문자열 1개 입력
+        System.out.printf("%s", str);
+    }
+}
