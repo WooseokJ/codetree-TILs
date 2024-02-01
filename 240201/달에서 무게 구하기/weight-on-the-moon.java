84617dffbe22aa9876be82d1d085ee25fbbc6480
Wooseok <wooseokbird@gmail.com>
2024-02-01 17:14:15 +0900

    Title: 달에서 무게 구하기, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240201/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 0.165;
+        double b = 2.145;
+
+        System.out.printf("13 * %.6f = %.6f",a,b);
+    }
+}
