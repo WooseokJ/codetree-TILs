a457245fcde3dc88f7394400e33b2ef20ef24670
Wooseok <wooseokbird@gmail.com>
2024-02-02 10:22:27 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240202/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char c = str.charAt(0);
+        String result;
+        switch (c) {
+            case "S" : 
+            result = "Superior";
+            break;
+            case "A":
+            result = "Excellent";
+            break;
+            case "B":
+            result = "Good";
+            break;
+            case "C":
+            result = "Usually";
+            break;
+            case "D":
+            result = "Effot";
+            break;
+            default:
+            result = "Failure";
+        }
+        System.out.println(result);
+        
+
+    }
+}
