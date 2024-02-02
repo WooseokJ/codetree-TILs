2013d7467abbbda9b041bd212e81a646864d79e3
Wooseok <wooseokbird@gmail.com>
2024-02-02 10:54:39 +0900

    Title: 시력 검사, Time: 158ms, Memory: 11MB, Status: Passed - CodeTree

--- 240202/시력 검사/eye-test.java
+++ 240202/시력 검사/eye-test.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double a= sc.nextDouble();
         double b= sc.nextDouble();
-        if ( (a > 1) && (b > 1)) {
+        if ( (a >= 1) && (b >= 1)) {
             System.out.println("High");
-        } else if((a > 0.5) && (b > 0.5)) {
+        } else if((a >= 0.5) && (b >= 0.5)) {
             System.out.println("Middle"); 
         } else {
             System.out.println("Low");
