65e61efa81acc74ee03e6b33d957ce32ee4657c6
Wooseok <wooseokbird@gmail.com>
2024-02-02 10:14:13 +0900

    Title: 살 수 있는 물건, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240202/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int book = 3000;
+        int mask = 1000;
+        int a = sc.nextInt();
+        if(a >= book) {
+            System.out.println("book");
+        } else {
+            System.out.println("mask");
+        } 
+    }
+}
