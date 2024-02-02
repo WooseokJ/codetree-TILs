814c89dda5c7a381a0c2ef1136a52e44a9214e9d
Wooseok <wooseokbird@gmail.com>
2024-02-02 10:43:27 +0900

    Title: 비교 연산, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/비교 연산/comparison-operator.java
+++ 240202/비교 연산/comparison-operator.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a>=b);
-        System.out.println(a>b);
-        System.out.println(b>=a);
-        System.out.println(b>a);
-        System.out.println(a==b);
-        System.out.println(a!=b);
+        System.out.println(a>=b ? 1 : 0);
+        System.out.println(a>b ? 1 : 0);
+        System.out.println(b>=a ? 1 : 0);
+        System.out.println(b>a ? 1 : 0);
+        System.out.println(a==b ? 1 : 0);
+        System.out.println(a!=b ? 1 : 0);
     }
 }
