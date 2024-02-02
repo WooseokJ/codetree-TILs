228939686ac72c2a17a6981f4194f1e148109ed3
Wooseok <wooseokbird@gmail.com>
2024-02-02 10:31:24 +0900

    Title: 최대 2번의 연산, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/최대 2번의 연산/up-to-2-calculations.java
+++ 240202/최대 2번의 연산/up-to-2-calculations.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        while( a % 2 == 0) {
+        if( a % 2 == 0) {
             a /= 2;
         }
         if (a % 2 != 0 ) {
