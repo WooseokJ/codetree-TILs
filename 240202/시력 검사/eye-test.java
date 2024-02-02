94a729f2817bfa42d8dec2cb2bd7d4b5dc0c2494
Wooseok <wooseokbird@gmail.com>
2024-02-02 10:54:18 +0900

    Title: 시력 검사, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240202/시력 검사/eye-test.java
+++ 240202/시력 검사/eye-test.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        double a= sc.nextInt();
-        double b= sc.nextInt();
-        if ( a > 1 && b > 1) {
+        double a= sc.nextDouble();
+        double b= sc.nextDouble();
+        if ( (a > 1) && (b > 1)) {
             System.out.println("High");
-        } else (a > 0.5 && b > 0.5) {
+        } else if((a > 0.5) && (b > 0.5)) {
             System.out.println("Middle"); 
-        } else{
+        } else {
             System.out.println("Low");
         } 
     }
