ccf2eca26880ab7c42f35158be39694211f4e02f
Wooseok <wooseokbird@gmail.com>
2024-02-04 14:36:20 +0900

    Title: 감소시키며 출력하기 2, Time: 116ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240204/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int start = 26;
+        int end = 10;
+        
+        while(start >= end) {
+            System.out.printf("%d ", start);
+            start-=1;
+        }
+    }
+}
