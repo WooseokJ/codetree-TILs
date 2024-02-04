325e39bb682848b0af1b97f8050f68653b947f52
Wooseok <wooseokbird@gmail.com>
2024-02-04 19:53:54 +0900

    Title: 홀수만 더하기, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240204/홀수만 더하기/add-only-odd-numbers.java
+++ 240204/홀수만 더하기/add-only-odd-numbers.java
@@ -2,13 +2,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int sum = 0 ;
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+        int sum = 0 ;
+
         for(int i = 0 ; i<n; i++) {
             int a = sc.nextInt();
-            if( a % 2 != 0 || a % 3 == 0) {
-                sum+=i;
+            if( (a % 2 != 0) && (a % 3 == 0)) {
+                sum+=a;
             }
         }
         System.out.printf("%d", sum);
