14c9e03f98bcfdf32bf80f638b21dfa05674fc2a
Wooseok <wooseokbird@gmail.com>
2024-02-04 22:25:22 +0900

    Title: 몫 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240204/몫 출력하기/print-share.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0 ;
+        while(true) {
+            int a= sc.nextInt();
+
+            if( a % 2 == 0 ) {
+                System.out.println(a / 2);
+                cnt++;
+                if( cnt == 3) {
+                    break;
+                }
+            }
+        }
+    }
+}
