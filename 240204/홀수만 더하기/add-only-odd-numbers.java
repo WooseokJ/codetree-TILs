6a3dca5b1e269128541840c3c2595419c2477226
Wooseok <wooseokbird@gmail.com>
2024-02-04 19:45:26 +0900

    Title: 홀수만 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240204/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int sum = 0 ;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i<=n; i++) {
+            int a = sc.nextInt();
+            if( a % 2 != 0 || a % 3 == 0) {
+                sum+=i;
+            }
+        }
+        System.out.printf("%d", sum);
+
+    }
+}
