b2a7f8b9d3da45b75584402be4390402eb3ed0b7
Wooseok <wooseokbird@gmail.com>
2024-02-04 22:40:39 +0900

    Title: 2의 거듭제곱수, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240204/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int cnt = 0;
+        while(true) {
+            n /= 2;
+            cnt++;
+            if( n == 1) {
+                break;
+            }
+            
+        }
+        System.out.printf("%d", cnt);
+    }
+}
