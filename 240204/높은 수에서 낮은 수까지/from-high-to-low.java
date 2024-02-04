611fe0386d62ca0976902655fbef998777afc039
Wooseok <wooseokbird@gmail.com>
2024-02-04 14:57:18 +0900

    Title: 높은 수에서 낮은 수까지, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240204/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 240204/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -5,8 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        for(int i = b ; i >= a ; i--) {
+        if( a < b ) {
+            for(int i = b ; i >= a ; i--) {
+            System.out.printf("%d ", i);
+            }
+        } else{ 
+            for(int i = a ; i >= b ; i--) {
             System.out.printf("%d ", i);
         }
+        }
+      
     }
 }
