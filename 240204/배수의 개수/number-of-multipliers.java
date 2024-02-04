ff01c4255805cea17b06fbd941211bd3c0a1367b
Wooseok <wooseokbird@gmail.com>
2024-02-04 19:37:52 +0900

    Title: 배수의 개수, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240204/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int cntThree = 0;
+        int cntFive = 0;
+        for(int i = 0 ; i < 10; i++) {
+            int a = sc.nextInt();
+            if(a % 3 == 0 ) {
+                cntThree+=1;
+            } else if(a % 5 == 0) {
+                cntFive+=1;
+            }
+        }
+        System.out.printf("%d %d", cntThree, cntFive);
+        
+    }
+}
