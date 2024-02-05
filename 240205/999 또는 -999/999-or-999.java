590ccc6e697b81935e03dec8a9d2925d98e9dc1d
Wooseok <wooseokbird@gmail.com>
2024-02-05 15:14:41 +0900

    Title: 999 또는 -999, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240205/999 또는 -999/999-or-999.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[100];
+        int maxVal = Integer.MIN_VALUE;
+        int minVal = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i < 100 ;i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 999 || arr[i] == -999) {
+                break;
+            }
+            if(arr[i] > maxVal) {
+                maxVal = arr[i];
+            } else {
+                minVal = arr[i];
+            }
+        }
+        System.out.printf("%d %d", maxVal, minVal);
+    }
+}
