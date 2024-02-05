66026bca4295b3600ab0e7c56c33fd2c515e7c5f
Wooseok <wooseokbird@gmail.com>
2024-02-06 00:43:27 +0900

    Title: a로 채워넣기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/a로 채워넣기/filling-with-a.java
+++ 240206/a로 채워넣기/filling-with-a.java
@@ -4,7 +4,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String str= sc.next();
-        String s = str.substring(0,1) + "a" + str.substring(2);
+        String s = str.substring(0,1) + "a" + str.substring(2, str.length() -2) + "a" + str.substring(str.length()-1);
+
         System.out.println(s);
     }
 }
