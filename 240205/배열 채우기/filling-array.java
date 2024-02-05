5731e259303ff7307d3a69be19555f500daed17a
Wooseok <wooseokbird@gmail.com>
2024-02-05 12:40:26 +0900

    Title: 배열 채우기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/배열 채우기/filling-array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        for(int i = 0 ; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0 ){
+                break;
+            }
+        }
+        for(int i = arr.length - 1 ; i >= 0 ; i --) {
+            if(arr[i] == 0) {
+                continue;
+            }
+            System.out.printf("%d ", arr[i]);
+        }
+        
+
+    }
+}
