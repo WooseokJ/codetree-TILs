2209af89da8c928069a3db49eea0275fbae8426b
Wooseok <wooseokbird@gmail.com>
2024-02-05 22:32:38 +0900

    Title: 문자열의 길이 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/문자열의 길이 출력하기/print-string's-range.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a= sc.next();
+        String b= sc.next();
+        System.out.printf("%d", (a.length() + b.length()));
+    }
+}
