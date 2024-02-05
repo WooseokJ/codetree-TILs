867c78fbad7755f6d4463b0c5eb6025629cc155f
Wooseok <wooseokbird@gmail.com>
2024-02-05 23:31:33 +0900

    Title: 문자열 전부 붙이기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240205/문자열 전부 붙이기/paste-all-string.java
+++ 240205/문자열 전부 붙이기/paste-all-string.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a= sc.nextInt();
-        int result = "";
+        String result = "";
         for(int i = 0; i < a; i ++) {
             result += sc.nextInt();
         }
