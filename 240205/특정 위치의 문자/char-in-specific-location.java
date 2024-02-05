f9a5652a0832f67c25d0dadbbea1cbfbebaeda56
Wooseok <wooseokbird@gmail.com>
2024-02-05 14:45:39 +0900

    Title: 특정 위치의 문자, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240205/특정 위치의 문자/char-in-specific-location.java
+++ 240205/특정 위치의 문자/char-in-specific-location.java
@@ -5,12 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         char[] arr = new char[]{'L', 'E', 'B', 'R', 'O', 'S'};
         String a = sc.next();
-
+        boolean status = true;
         for(int i = 0 ;i<6;i++) {
             if (arr[i] == a.charAt(0)) {
                 System.out.printf("%d",i);
-            }
+                status = false;
+            } 
         }
+        if(status) {
+            System.out.printf("None");
+        }
+
 
     }
 }
