a1fd5025a45aeeabce424ab5247854c32b18f20e
Wooseok <wooseokbird@gmail.com>
2024-02-06 16:51:12 +0900

    Title: 최대공약수 구하기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240206/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -4,13 +4,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        findGCD(n,m);
+        findGCD(n, m);
     }
     public static void findGCD(int n, int m) {
         int gcd = 0 ;
-        for(int i = 0; i <= Math.min(n,m);i++) {
-            if(n % i == 0 && m % i == 0)
+        for(int i = 1; i <= Math.min(n,m);i++) {
+            if(n % i == 0 && m % i == 0) {
                 gcd = i;
+            }
         }
         System.out.print(gcd);
     }
