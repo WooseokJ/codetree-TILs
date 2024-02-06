9cfcd3d3c5e88b802b81ef965d84557862e7f374
Wooseok <wooseokbird@gmail.com>
2024-02-07 02:33:05 +0900

    Title: 줄 세우기 2, Time: 220ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240207/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class User {
+    int kg;
+    int height;
+    int rank;
+    User(int k , int h, int r) {
+        kg = k;
+        height = h;
+        rank = r;
+    }
+    void print() {
+        System.out.printf("%d %d %d\n", kg, height, rank);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        User[] users = new User[n];
+        for(int i = 0; i < n; i++) {
+            int kg = sc.nextInt();
+            int height = sc.nextInt();
+            users[i] = new User(kg, height, i+1);
+        }
+
+        Arrays.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User a, User b) {
+                if(a.height == b.height) {
+                    return b.height - a.height;
+                }
+                return a.height - b.height;
+            }
+        });
+
+        for(User u: users)
+            u.print();
+
+    }
+}
