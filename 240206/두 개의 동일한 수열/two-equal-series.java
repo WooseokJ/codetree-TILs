4bee4bf0ccaa6f58388aefe384e475da5c8d9a3c
Wooseok <wooseokbird@gmail.com>
2024-02-06 17:45:39 +0900

    Title: 두 개의 동일한 수열, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/두 개의 동일한 수열/two-equal-series.java
+++ 240206/두 개의 동일한 수열/two-equal-series.java
@@ -4,8 +4,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr1 = int[n];
-        int[] arr2 = int[n];
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
 
         for(int i = 0 ; i < arr1.length;i++)
             arr1[i] = sc.nextInt();
