9b3ee3d06ec6cc043ce31b029d71588d6547061c
Wooseok <wooseokbird@gmail.com>
2024-02-06 16:49:08 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240206/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        findGCD(n,m);
+    }
+    public static void findGCD(int n, int m) {
+        int gcd = 0 ;
+        for(int i = 0; i <= Math.min(n,m);i++) {
+            if(n % i == 0 && m % i == 0)
+                gcd = i;
+        }
+        System.out.print(gcd);
+    }
+}
