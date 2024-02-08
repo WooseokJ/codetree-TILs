9172d843449e4a7aefb8952510ada243866bdd6c
Wooseok <wooseokbird@gmail.com>
2024-02-08 23:55:57 +0900

    Title: 방향에 맞춰 이동, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/방향에 맞춰 이동/move-in-direction.java
+++ 240208/방향에 맞춰 이동/move-in-direction.java
@@ -1,5 +1,7 @@
 import java.util.*;
 public class Main {
+    public static int nx,ny;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -8,18 +10,17 @@ public class Main {
         int[] dx = new int[]{1,0,-1,0};
         int[] dy = new int[]{0,-1,0,1};
         int x = 0; int y = 0;
-        int[] nx = new int[]{0};
-        int[] ny = new int[]{0};
+        
         for(int i = 0; i < n ; i++) {
             char str = sc.next().charAt(0);
             int dirNum = sc.nextInt();
             int inputNum = directInt(str);
-            nx[0] = x + (dx[inputNum] * dirNum);
-            ny[0] = y + (dy[inputNum] * dirNum);
-            x = nx[0];
-            y = ny[0];
+            nx = x + (dx[inputNum] * dirNum);
+            ny = y + (dy[inputNum] * dirNum);
+            x = nx;
+            y = ny;
         }
-        System.out.printf("%d %d", nx[0], ny[0]);
+        System.out.printf("%d %d", nx, ny);
     }
     public static int directInt(char str) {
         switch (str) {
