ecea5dcfe9fbf1e259143aa97ac5599afbf95cd2
Wooseok <wooseokbird@gmail.com>
2024-02-11 20:54:46 +0900

    Title: 특정 원소들의 합, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240211/특정 원소들의 합/sum-of-specific-elements.java
+++ 240211/특정 원소들의 합/sum-of-specific-elements.java
@@ -11,7 +11,7 @@ public class Main {
             }
         }
         for(int i = 0 ; i < 4; i++) {
-            for(int j = 0; j < i; j++) {
+            for(int j = 0; j <= i; j++) {
                 sum += arr[i][j];
             }
         }
